889e0525eefcc3fb1112c910e956df5b861c98e3
IHakoda <Hakoda.AK@gmail.com>
2022-04-02 02:18:15 +0300

    Popover
    Popover to introduce wonderful people

--- js/main.js
+++ js/main.js
@@ -72,4 +72,13 @@ $(document).ready(function() {
         scrollTop: $("#iOSDev-Section").offset().top
     }, 2000);
   });
+
+  // Popover
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+  var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
+    trigger: 'focus'
+  })
 });
