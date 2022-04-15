d80e15ea18e19fa38893f324b64334b9bb80a26c
IHakoda <Hakoda.AK@gmail.com>
2022-04-15 12:43:20 +0300

    Small changes
    Small changes

--- js/firebase-messaging-sw.js
+++ js/firebase-messaging-sw.js
@@ -1,19 +1,21 @@
+$(document).ready(function() {
+  import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-messaging.js";
 
-import { getMessaging, getToken } from "https://www.gstatic.com/firebasejs/9.6.11/firebase-messaging.js";
+  // Get registration token. Initially this makes a network call, once retrieved
+  // subsequent calls to getToken will return from cache.
+  const messaging = getMessaging();
+  getToken(messaging, { vapidKey: 'BJzh2i1gSOwRlcvb7Azkpdsa8ytbTDqrSzRYnlgT2sY3o-J7f6Js-dKvktPFEEwokZmyk489bB_NjYp5Bp84afY' }).then((currentToken) => {
+    if (currentToken) {
+      // Send the token to your server and update the UI if necessary
+      // ...
+    } else {
+      // Show permission request UI
+      console.log('No registration token available. Request permission to generate one.');
+      // ...
+    }
+  }).catch((err) => {
+    console.log('An error occurred while retrieving token. ', err);
+    // ...
+  });
 
-// Get registration token. Initially this makes a network call, once retrieved
-// subsequent calls to getToken will return from cache.
-const messaging = getMessaging();
-getToken(messaging, { vapidKey: 'BJzh2i1gSOwRlcvb7Azkpdsa8ytbTDqrSzRYnlgT2sY3o-J7f6Js-dKvktPFEEwokZmyk489bB_NjYp5Bp84afY' }).then((currentToken) => {
-  if (currentToken) {
-    // Send the token to your server and update the UI if necessary
-    // ...
-  } else {
-    // Show permission request UI
-    console.log('No registration token available. Request permission to generate one.');
-    // ...
-  }
-}).catch((err) => {
-  console.log('An error occurred while retrieving token. ', err);
-  // ...
 });
